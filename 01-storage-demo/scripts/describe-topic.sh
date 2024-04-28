7f43372e405637817fdb8e147930e15533e7fb21
Nandhakumar Ramanujan <nandha88@gmail.com>
2024-04-28 19:22:31 +0530

    topic created rectified port

--- 01-storage-demo/scripts/describe-topic.sh
+++ 01-storage-demo/scripts/describe-topic.sh
@@ -1,2 +1,2 @@
 export KAFKA_HOME=/home/nandha88/IdeaProjects/confluent-community-7.6.1/confluent-7.6.1
-$KAFKA_HOME/bin/kafka-topics --describe  --zookeeper localhost:2181 --topic invoice
+$KAFKA_HOME/bin/kafka-topics --describe  --bootstrap-server localhost:9092 --topic invoice
