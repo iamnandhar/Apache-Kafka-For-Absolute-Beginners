5e9ea2e4b69de5c768ed0f954e018a8a2e6c2162
Nandhakumar Ramanujan <nandha88@gmail.com>
2024-04-28 15:39:47 +0530

    Modified bat to sh for RHEL

---
+++ 01-storage-demo/scripts/describe-topic.sh
@@ -0,0 +1,2 @@
+export KAFKA_HOME=/home/nandha88/IdeaProjects/confluent-community-7.6.1/confluent-7.6.1
+$KAFKA_HOME/bin/kafka-topics --describe  --zookeeper localhost:2181 --topic invoice
---
+++ 01-storage-demo/scripts/topic-create.sh
@@ -0,0 +1,2 @@
+export KAFKA_HOME=/home/nandha88/IdeaProjects/confluent-community-7.6.1/confluent-7.6.1
+$KAFKA_HOME/bin/kafka-topics --create --zookeeper localhost:2181 --topic invoice --partitions 5 --replication-factor 3 --config segment.bytes=1000000
