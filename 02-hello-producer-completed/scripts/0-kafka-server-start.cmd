85d2382196c283315b7cb2a6c6f7238055fd7c9a
Nandhakumar Ramanujan <nandha88@gmail.com>
2024-05-12 18:08:31 +0530

    sh file conversion

--- 02-hello-producer-completed/scripts/0-kafka-server-start.cmd
+++ 02-hello-producer-completed/scripts/0-kafka-server-start.cmd
@@ -1,1 +1,3 @@
-%KAFKA_HOME%\bin\windows\kafka-server-start.bat %KAFKA_HOME%\etc\kafka\server-0.properties
+export KAFKA_HOME=/home/nandha88/IdeaProjects/confluent-community-7.6.1/confluent-7.6.1
+echo $KAFKA_HOME
+$KAFKA_HOME/bin/kafka-server-start $KAFKA_HOME/etc/kafka/server-0.properties
