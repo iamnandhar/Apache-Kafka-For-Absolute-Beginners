f3b388907252fe86659ccf7025e92d622baac796
Nandhakumar Ramanujan <nandha88@gmail.com>
2024-05-12 18:09:20 +0530

    added linux sh

---
+++ 02-hello-producer-completed/scriptssh/2-kafka-server-start.sh
@@ -0,0 +1,2 @@
+export KAFKA_HOME=/home/nandha88/IdeaProjects/confluent-community-7.6.1/confluent-7.6.1
+$KAFKA_HOME/bin/kafka-server-start $KAFKA_HOME/etc/kafka/server-2.properties
---
+++ 02-hello-producer-completed/scriptssh/describe-topic.sh
@@ -0,0 +1,2 @@
+export KAFKA_HOME=/home/nandha88/IdeaProjects/confluent-community-7.6.1/confluent-7.6.1
+$KAFKA_HOME/bin/kafka-topics --describe  --bootstrap-server localhost:9092 --topic invoice
